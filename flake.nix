939be8d2af3c068e1fe018edaca37114a6962023
Ignacio Perez <ignacio@feuer.me>
2026-07-16 16:26:17 -0300

    build(nix): bootstrap devenv Rust toolchain

--- flake.nix
+++ flake.nix
@@ -3,16 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "git+https://github.com/hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    devenv.url = "git+https://github.com/cachix/devenv";
+    nix2container = {
+      url = "git+https://github.com/nlewo/nix2container";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    mk-shell-bin.url = "git+https://github.com/rrbutani/nix-mk-shell-bin";
+    rust-overlay = {
+      url = "git+https://github.com/oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      imports = [ inputs.devenv.flakeModule ];
+
+      perSystem = { pkgs, ... }: {
+        devenv.shells.default = {
           packages = [
             pkgs.go
             pkgs.gopls
@@ -22,11 +41,17 @@
             pkgs.sqlc
           ];
 
-          shellHook = ''
-            echo "Agens dev shell (Go, just, golangci-lint)"
+          languages.rust = {
+            enable = true;
+            toolchainFile = ./rust-toolchain.toml;
+          };
+
+          enterShell = ''
+            echo "Agens dev shell (Go, Rust, just, golangci-lint)"
           '';
         };
 
         formatter = pkgs.nixpkgs-fmt;
-      });
+      };
+    };
 }
