c97c120cda9089abd994dbed60cbc3272d8adc78
Ignacio <67933444+0xErwin1@users.noreply.github.com>
2026-08-22 07:45:49 -0300

    feat(server): manage session worktrees (#13)

--- crates/agens-server/src/lib.rs
+++ crates/agens-server/src/lib.rs
@@ -7,6 +7,7 @@
 
 mod blocking;
 mod instance;
+mod worktrees;
 
 use std::os::unix::net::UnixListener;
 
@@ -14,6 +15,7 @@ use agens_core::HeadlessTurnCancellation;
 
 pub use blocking::{BlockingBoundary, BlockingError};
 pub use instance::{ServeInstance, ServeInstanceError};
+pub use worktrees::{SessionWorktrees, WorktreeError, WorktreeStatus};
 
 #[derive(Debug)]
 pub enum ServerError {
